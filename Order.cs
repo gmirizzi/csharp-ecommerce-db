39556e5560aa11df765033b7bb74fefe445b7914
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-12 14:52:16 +0200

    creata la classe order

--- Order.cs
+++ Order.cs
@@ -4,4 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 [Table("orders")]
 public class Order
 {
+    [Key]
+    public int OrderId { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public string Status { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
 }
