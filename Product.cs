990eebc3afcb1e03e99b060b82c6787d411c03f5
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-12 15:06:40 +0200

    creata la classe product

--- Product.cs
+++ Product.cs
@@ -1,3 +1,15 @@
-﻿public class Product
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("products")]
+public class Product
 {
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Column(TypeName = "text")]
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public List<OrderProduct> OrderProducts { get; set; }
 }
