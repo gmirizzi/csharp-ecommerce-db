a781c16e2c0848c631cd780366c3d5b32eac6ecd
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-12 14:43:17 +0200

    creata la classe customer

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("customers")]
+[Index(nameof(Email), IsUnique = true)]
+public class Customer
+{
+    [Key]
+    public int CustomerId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Surname { get; set; }
+    [Required]
+    public string Email { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ Order.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("orders")]
+public class Order
+{
+}
